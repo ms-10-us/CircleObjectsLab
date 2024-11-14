36938ff829da8e9702af13bd4ab8694ce3f3157f
Mazen Saber <mazensaber1996@gmail.com>
2024-11-14 06:16:27 -0500

    Circle Objects Lab - Mazen Saber

---
+++ CircleObjectsLab/CircleObjectsLab/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleObjectsLab
+{
+    internal class Circle
+    {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double CalculateDiameter()
+        {
+            double diameter = 2 * radius;
+            return diameter;
+        }
+
+        public double CalculateCircumference()
+        {
+            double circumference = 2 * Math.PI * radius;
+            return circumference;
+        }
+
+        public double CalculateArea()
+        {
+            double area = Math.PI * radius * radius;
+            return area;
+        }
+
+        public void Grow()
+        {
+            radius = 2 * radius;
+            double diameter = CalculateDiameter();
+            double circumference = CalculateCircumference();
+            double area = CalculateArea();
+
+            Console.WriteLine($"Diameter: {diameter}");
+            Console.WriteLine($"Circumference: {circumference}");
+            Console.WriteLine($"Area: {area}");
+        }
+
+        public double GetRadius()
+        {
+            return radius;
+        }
+    }
+}
---
+++ CircleObjectsLab/CircleObjectsLab/Program.cs
@@ -0,0 +1,52 @@
+﻿using CircleObjectsLab;
+
+Console.WriteLine("Circle Objects Lab!");
+
+Console.WriteLine();
+
+double radius = 0;
+Console.WriteLine("Entrer circle's radius");
+while (true)
+{
+    try
+    {
+        string userAnswer = "";
+
+        radius = Convert.ToDouble(Console.ReadLine());
+        Circle circleObject = new Circle(radius);
+        double diameter = circleObject.CalculateDiameter();
+        double circumference = circleObject.CalculateCircumference();
+        double area = circleObject.CalculateArea();
+        Console.WriteLine($"Diameter: {diameter}");
+        Console.WriteLine($"Circumference: {circumference}");
+        Console.WriteLine($"Area: {area}");
+
+        Console.WriteLine();
+
+        while (true)
+        {
+            Console.WriteLine("Should the circle grow (y/n)?");
+            userAnswer = Console.ReadLine();
+
+            if (userAnswer.ToLower().Trim() == "y")
+            {
+                Console.WriteLine("The circle is magically growing");
+                circleObject.Grow();
+            }
+            else if (userAnswer.ToLower().Trim() == "n")
+            {
+                Console.WriteLine($"Goodbye! The circle's final radius is {circleObject.GetRadius()}");
+                Environment.Exit(0);
+            }
+        }
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Please enter a valid radius");
+        continue;
+    }
+}
+
+
+
+
